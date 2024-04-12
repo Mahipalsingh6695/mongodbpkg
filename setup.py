c84de7bcc024e074b25401b2f015cb7bac887800
Mahipal Singh <mahipalsinghchauhan1995@gmail.com>
2024-04-12 08:56:51 +0530

    update

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open('README.md', 'r', encoding='utf-8') as f:
 __version__ = "0.0.4"
 REPO_NAME = "mongodbconnectorpkg"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "jarvis6695"
+AUTHOR_EMAIL = "mahipalsinghchauhan1995@gmail.com"
 
 setup(
     name=PKG_NAME,
